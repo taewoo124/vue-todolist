e06fe44ad5ace7f9ec963884cce6397ce9380609
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-11-23 13:32:21 +0900

    feat: 추가, 삭제, 완료 기능

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from "vue";
+
+let id = 0;
+
+const inputValue = ref("");
+const todos = ref([]);
+const hideCompleted = ref(false);
+
+const addTodo = () => {
+  todos.value.push({ id: id++, value: inputValue.value, done: false });
+  inputValue.value = "";
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const filterdTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Vue TodoList</h1>
+    <form @submit.prevent="addTodo">
+      <div class="input-button">
+        <input v-model="inputValue" @keydown.enter="addTodo" />
+        <button>추가</button>
+      </div>
+      <div class="list-container">
+        <li v-for="todo in filterdTodos" :key="todo.id">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }">{{ todo.value }}</span>
+          <button @click="removeTodo(todo)">X</button>
+        </li>
+      </div>
+    </form>
+    <button @click="hideCompleted = !hideCompleted">
+      {{ hideCompleted ? "Show All!" : "Hide Completed!" }}
+    </button>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.done {
+  text-decoration: line-through;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.input-button {
+  display: flex;
 }
 </style>
